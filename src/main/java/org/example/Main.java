2ecca4ff20fb39c25fc6b1f194bb90fb16e0f6db
Marco Aurelio Souza Mangan <10067320@facin.portoalegre.pucrsnet.br>
2023-11-09 10:29:47 -0300

    toString

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -24,11 +24,38 @@ class BinaryTree {
         return this.root == null;
     }
 
+    @Override
+    public String toString() {
+        return toString0(this.root);
+    }
+
+    private String toString0(Node node) {
+        if (node == null)
+            return "";
+
+        return toString0(node.left)
+                + node.value
+                + toString0(node.right);
+    }
+
+
 }
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println(" Binary Tree!");
 
         BinaryTree tree = new BinaryTree();
+
+        Node plus = new Node("+");
+        Node two = new Node("2");
+        Node three = new Node("3");
+
+        plus.left = two;
+        plus.right = three;
+
+        tree.root = plus;
+
+        System.out.println(tree.isEmpty());
+        System.out.println(tree.toString());
     }
 }
---
+++ src/test/java/org/example/BinaryTreeTest.java
@@ -0,0 +1,100 @@
+package org.example;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+    public class BinaryTreeTest {
+        @Test
+        public void newTreeIsEmpty() {
+            BinaryTree tree = new BinaryTree();
+
+            boolean expected = true;
+            boolean actual = tree.isEmpty();
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void checkToString() {
+            BinaryTree tree = new BinaryTree();
+
+            /**   root  -+
+             *           |
+             *           v
+             *           +
+             *         /   \
+             *        2     3
+             *
+             *        2+3
+             */
+            Node plus = new Node("+");
+            Node two = new Node("2");
+            Node three = new Node("3");
+            plus.left = two;
+            plus.right = three;
+            tree.root = plus;
+
+            String expected = "2+3";
+            String actual = tree.toString();
+
+            assertEquals(expected, actual);
+        }
+
+
+        @Test
+        public void checkSingleNode() {
+            BinaryTree tree = new BinaryTree();
+
+            /**   root  -+
+             *           |
+             *           v
+             *           8
+             *
+             *           "8"
+             */
+            tree.root = new Node("8");
+
+            String expected = "8";
+            String actual = tree.toString();
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        public void checkTo() {
+            BinaryTree tree = new BinaryTree();
+
+            /**    root  ----+
+             *               |
+             *               v
+             *               *
+             *             /   \
+             *           +      5
+             *         /   \
+             *        2     3
+             *
+             *        (2+3)*5
+             */
+            Node plus = new Node("+");
+            Node two = new Node("2");
+            Node three = new Node("3");
+            Node times = new Node("*");
+            Node five = new Node("5");
+            plus.left = two;
+            plus.right = three;
+
+            times.left = plus;
+            times.right = five;
+
+            tree.root = times;
+
+            String expected = "(2+3)*5";
+            String actual = tree.toString();
+
+            assertEquals(expected, actual);
+        }
+
+    }
+
